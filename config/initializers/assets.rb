ba4cfca24d6030a3a81bc92caead02942416de08
Resing1990 <33636566+Resing1990@users.noreply.github.com>
2019-01-29 21:55:29 +0100

    first commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,14 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules' 'fonts' 'audio' 'video')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+Rails.application.config.assets.precompile += %w( main/main.css main/main.js dash/main.css dash/main.js game_desktop/main.css game_desktop/main.js game_mobile/main.css game_mobile/main.js backoffice/main.css game_desktop/load_media.css)
