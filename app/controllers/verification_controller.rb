eae4dcee92a620dce12285955fa5548441bba1cc
Summan Rashid <suman.rashid@phaedrasolutions.com>
2019-03-06 21:06:51 +0500

    Debugging

--- app/controllers/verification_controller.rb
+++ app/controllers/verification_controller.rb
@@ -1,16 +1,13 @@
 class VerificationController < ApplicationController
 
     def token
-        debugger
         @admin =Admin.where(vid_token: params[:token]).first
     end
 
 
     def verify_token
-        debugger
         @admin =Admin.where(token: params[:token]).first
         if @admin
-            debugger
             redirect_to edit_admin_path(v_id: @admin.vid_token)
         else
             redirect_to verification_token_url(@admin.vid_token)
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,6 @@ class AdminMailer < ApplicationMailer
     default from: 'support@gmail.com'
   
     def offer_to_mail(admin)
-        debugger
         @admin = admin
         mail to: @admin.email , subject:"Contact Email"
     end
