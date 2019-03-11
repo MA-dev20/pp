96c2bb41fee07bf7d2e1428f7ce54cb03bb3cc80
= <hammad417@gmail.com>
2019-03-11 22:55:32 +0500

    Integrate Product Page and Header

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resource :cards
   get 'landing/index'
   get 'landing/price', to: 'landing#price', as: 'price'
+  get 'landing/product', to: 'landing#product', as: 'product'
   root 'landing#index'
   patch 'verification/verify_token' ,to: 'verification#verify_token' , as:'verify_token'
   get 'admins/register', to: 'landing#register', as: 'register'
--- db/migrate/20190311120139_create_cards.rb
+++ db/migrate/20190311120139_create_cards.rb
@@ -1,8 +1,8 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.belongs_to :admin, foreign_key: true
-      t.string :card_id
+      t.references :admin
+      t.string :card_token_id
       t.timestamps
     end
   end
