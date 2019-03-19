38d791bf0d964715a6b0fc915feffd0b02e35d25
Hammad Maqbool <hammad417@gmail.com>
2019-03-19 18:35:11 +0500

    Plan redirection issue fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,15 @@ class ApplicationController < ActionController::Base
 
   def check_expiration_date
     if admin_signed_in?
-       if current_admin.plans.blank? && Time.now >= current_admin.expiry
-        redirect_to price_path
-       end
+      if current_admin.plan_id.blank? && Time.now >= current_admin.expiry
+        if current_admin.cards.blank?
+          flash[:danger] = "Please add card details to continue using application"
+          return redirect_to dash_admin_billing_path 
+        else
+          flash[:danger] = "Please Select Plan type"
+          return redirect_to price_path
+        end
+      end
     end
   end
   
