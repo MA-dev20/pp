72a9918de71b60bca3885f1c53a3b4ee4da02d0e
Hammad Maqbool <hammad417@gmail.com>
2019-04-09 12:28:48 +0500

    fixes.

--- app/assets/javascripts/main/main.js
+++ app/assets/javascripts/main/main.js
@@ -8,30 +8,3 @@
  *= require main/script
  *= require cable
  */
-
-$("#loginForm").on("submit", function(e){
-  e.preventDefault()
-  var email = $("#loginEmail").val(),
-  password = $("#loginPassword").val();
-  ajaxLogin(email, password)
-})
-function ajaxLogin(email, password){
-  $(".email-error").hide()
-  $(".password-error").hide()
-  $.ajax({
-    url: '/admins/sign_in',
-      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-    data: {admin: {email: email, password: password}},
-    type: "POST", 
-    success: function(res){
-      if(res.response == "error"){
-        if(res.errors.email)
-         $(".email-error").show()
-        if(res.errors.password)
-         $(".password-error").show()
-      }else{
-      	window.location.href = "/admin/dash"
-      }
-    }
-  });
-}
