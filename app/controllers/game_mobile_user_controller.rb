a73f5957c5450d20c0a6429d04595c4f37646032
Hammad Maqbool <hammad417@gmail.com>
2019-04-15 17:40:47 +0500

    Fix not request if user already present.

--- app/controllers/game_mobile_user_controller.rb
+++ app/controllers/game_mobile_user_controller.rb
@@ -3,6 +3,7 @@ class GameMobileUserController < ApplicationController
   before_action :authenticate_user!, :set_user, except: [:welcome, :new, :create,:reject_user ,:accept_user]
   before_action :set_turn, only: [:turn, :play, :rate, :rated, :rating]
   # before_action :pop_up ,only: :create
+  before_action :reset_session_of_already_user, only: [:new, :welcome]
   layout 'game_mobile'
 
     
@@ -37,6 +38,7 @@ class GameMobileUserController < ApplicationController
         if TeamUser.where(user_id: @user.id, team_id: @game1.team_id).count == 0
           TeamUser.create(user_id: @user.id, team_id: @game1.team_id)
         end
+        session[:user_already] = true
         sign_in(@user)
         redirect_to gmu_new_avatar_path
       else
@@ -170,12 +172,19 @@ class GameMobileUserController < ApplicationController
     
   def create_turn
     @game1 = Game.find(session[:game_session_id])
+    @admin = @game1.admin
+    @game = @game1
     @word = Word.first(50).sample(5).first if @game1.admin.admin_subscription_id.nil?
     @word = Word.all.sample(5).first if @word.nil?
     if @game1.active
       session.delete(:game_session_id)
       sign_in(@game1)
-      ActionCable.server.broadcast "count_#{@game1.id}_channel", count: 'true', counter: @game1.turns.count.to_s
+      if session[:user_already]
+        create_turn_against_user(current_user, @admin)
+        ActionCable.server.broadcast "count_#{@game1.id}_channel", count: 'true', counter: @game1.turns.count.to_s
+      else
+        ActionCable.server.broadcast "count_#{@game1.id}_channel", count: 'true', counter: @game1.turns.count.to_s
+      end
       redirect_to gmu_wait_path
     else
       flash[:danger] = 'Das Spiel ist schon beendet!'
@@ -189,9 +198,11 @@ class GameMobileUserController < ApplicationController
     @admin = Admin.find(@game.admin_id)
     turn =  Turn.where(user_id:  current_user.id, game_id:  @game.id, admin_id: @admin.id).first
     if (turn.nil?) 
-      ActionCable.server.broadcast "count_#{@game.id}_channel", count: 'wait-user', game_state: 'wait' ,game_id: current_game.id, counter: @game.turns.count.to_s, 
-      user_fname: current_user.fname, user_lname: current_user.lname,
-      user_avatar: current_user.avatar.url , user_id: current_user.id
+      if !session[:user_already]
+        ActionCable.server.broadcast "count_#{@game.id}_channel", count: 'wait-user', game_state: 'wait' ,game_id: current_game.id, counter: @game.turns.count.to_s, 
+        user_fname: current_user.fname, user_lname: current_user.lname,
+        user_avatar: current_user.avatar.url , user_id: current_user.id
+      end
     end
   end
     
@@ -259,6 +270,10 @@ class GameMobileUserController < ApplicationController
       turn =  Turn.where(user_id:  user.id, game_id:  @game.id, admin_id: admin.id).first
       @turn = Turn.new(user_id: user.id, game_id: @game1.id, word_id: @word.id, play: true, played: false, admin_id: admin.id)
       @turn.save! if turn.nil?
+    end
+
+    def reset_session_of_already_user
+      session[:user_already] = nil
     end
 
 
