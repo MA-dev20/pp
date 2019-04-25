43c728c12c0a4b7d607c5ea582b3163123020803
Hammad Maqbool <hammad417@gmail.com>
2019-04-25 12:02:28 +0500

    Png css changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
 	def create_image_of_html(html, js_path, css_path)
-		debugger
 		kit = IMGKit.new(html, :quality => 50)
 		kit.stylesheets << css_path
 		kit.javascripts << js_path
