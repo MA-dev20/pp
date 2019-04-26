2ebca1685686dfe918290710b9bc89e08f85e4a3
Hammad Maqbool <hammad417@gmail.com>
2019-04-26 19:06:13 +0500

    Design fixes.

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -3,7 +3,7 @@ class Turn < ApplicationRecord
   belongs_to :user, required: false
   belongs_to :admin, required: false
 
-  has_one :turn_rating
+  has_one :turn_rating, dependent: :destroy
   has_many :ratings, dependent: :destroy
   TURN_QUERY = 'users.*,(select count(*) from turns t1 where t1.user_id=users.id and place=1) as gold_count, (select count(*) from turns t1 where t1.user_id=users.id and place=2) as silver_count, (select count(*) from turns t1 where t1.user_id=users.id and place=3) as bronze_count'
     
