555397084e0b9952b5b4929b9a0cd96c0efe63c2
Hammad Maqbool <hammad417@gmail.com>
2019-05-23 10:52:06 +0500

    Revert.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'ws://peterpitch.com/cable'
   # config.action_cable.allowed_request_origins = ['http://peterpitch.de', 'https://peterpitch.de', 'http://www.peterpitch.de', 'https://www.peterpitch.de', 'http://peterpitch.com', 'http://www.peterpitch.com', 'https://peterpitch.com', 'https://www.peterpitch.com']
-  config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
+  # config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
