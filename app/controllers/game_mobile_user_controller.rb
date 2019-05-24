7a390e98239eed3b0ef55726e948271c93e958ac
Hammad Maqbool <hammad417@gmail.com>
2019-05-24 10:11:09 +0500

    Fix design issues.

--- app/controllers/game_mobile_user_controller.rb
+++ app/controllers/game_mobile_user_controller.rb
@@ -106,7 +106,7 @@ class GameMobileUserController < ApplicationController
     elsif @admin.plan_users?
       @user.update_attributes(status: 'accepted')
       create_turn_against_user(@user, @admin, "accepted")
-      ActionCable.server.broadcast  "count_#{@game1.id}_channel", count: 'true', counter: @game.turns.where(status: "accepted").playable.count.to_s, modal: false, user_id: params[:user_id], new: 'true', user_pic: @user.avatar.quad.url
+      ActionCable.server.broadcast  "count_#{@game1.id}_channel", count: 'true', counter: @game.turns.where(status: "accepted").playable.count.to_s, modal: false,new: 'true', user_id: params[:user_id], new: 'true', user_pic: @user.avatar.quad.url
       if((@game.turns.select{|turn| turn if !turn.user.nil? && turn.user.accepted?}.count) > @admin.plan_users )
         if @user.admin.plan_type.eql?("year")
           @admin.update_attributes(plan_users: @admin.plan_users + 1 )
