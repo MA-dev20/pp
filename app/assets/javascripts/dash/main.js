ad49f2f73e83cc7de9ad1b39546493088cd77c0e
Resing1990 <33636566+Resing1990@users.noreply.github.com>
2019-06-03 16:50:10 +0200

    updated javascript -> dash_admin

--- app/assets/javascripts/dash/main.js
+++ app/assets/javascripts/dash/main.js
@@ -1,14 +1,16 @@
 /*
+ *= require rails-ujs
+ *= require jquery3
  *= require activestorage
  *= require turbolinks
- *= require jquery3
  *= require popper
  *= require bootstrap
+ *= require cable
+ *= require bootstrap.bundle.min.js
  *= require Chart.min
  *= require dash/perfect-scrollbar.min
  *= require dash/classie.min
  *= require dash/selectFx.min
- *= require cable
  *= require bootstrap-tagsinput.js
  *= require typeahead.bundle.js
  *= require social-share-button
