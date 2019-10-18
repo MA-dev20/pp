d4eb77c2d065c75f0c457acf5928c5a30ad3718e
shakeelphaedra <shakeel@phaedrasolutions.com>
2019-10-18 13:43:02 +0500

    check_expiration_date commented out

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,5 @@
 class AdminsController < ApplicationController
-  skip_before_action :check_expiration_date
+  # skip_before_action :check_expiration_date
   before_action :require_root, :set_vars
   def destroy
     if @admin.destroy
