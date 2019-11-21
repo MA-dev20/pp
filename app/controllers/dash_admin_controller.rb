5d0deebfea129a8c8145a0317c61b50a340aeb06
shakeel rauf <shakeel@phaedrasolutions.com>
2019-11-21 17:22:06 +0500

    User stats fixes.

--- app/controllers/dash_admin_controller.rb
+++ app/controllers/dash_admin_controller.rb
@@ -1,6 +1,6 @@
 class DashAdminController < ApplicationController
   before_action :authenticate_admin!, :set_admin , :is_activated, unless: :skip_action?
-  before_action :set_team, only: [:games, :team_stats, :team_users, :user_stats, :compare_user_stats,:team_stats_share]
+  before_action :set_team, only: [:games, :team_stats, :team_users, :compare_user_stats,:team_stats_share]
   before_action :set_user, only: [:user_stats, :compare_user_stats]
   # skip_before_action :check_expiration_date, only: [:billing, :user_list]
   include ApplicationHelper
