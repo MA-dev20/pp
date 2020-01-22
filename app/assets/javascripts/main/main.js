d67043baa50530a04a6dd8661e7adb5ed7b14b09
Resing1990 <33636566+Resing1990@users.noreply.github.com>
2020-01-22 10:54:06 +0100

    app/

--- app/assets/javascripts/main/main.js
+++ app/assets/javascripts/main/main.js
@@ -1,12 +1,11 @@
 /*
- *= require main/bodymovin
- *= require main/confetti
  *= require jquery3
  *= require jquery_ujs
  *= require main/typed.min
  *= require main/wow
  *= require main/scrollspy
  *= require main/bootstrap.bundle.min
+ *= require main/bodymovin
  *= require main/script
  *= require cable
  */
