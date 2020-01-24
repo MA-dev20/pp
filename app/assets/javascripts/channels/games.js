5ebfb8a077a209ae83107808f7580cf374f59d84
shakeelphaedra <shakeel@phaedrasolutions.com>
2020-01-24 18:18:21 +0500

    Update games.js

--- app/assets/javascripts/channels/games.js
+++ app/assets/javascripts/channels/games.js
@@ -59,7 +59,7 @@ jQuery(document).ready(function() {
       }
     });
     $(window).bind('beforeunload', function(){
-      if((location.pathname.split("user")[0]=="/mobile/" && location.pathname.split("user")[1]=="/game/bestlist"){
+      if(location.pathname.split("user")[0]=="/mobile/" && location.pathname.split("user")[1]=="/game/bestlist"){
         console.log('nothing')
       }
       else {
