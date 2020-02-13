bffebf92d59abf70891a634e8ff8fa77e5853065
Resing1990 <33636566+Resing1990@users.noreply.github.com>
2020-02-13 11:44:23 +0100

    meta optimation

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -43,58 +43,9 @@ class LandingController < ApplicationController
   def contact
   end
     
-  def register
-    @admin = Admin.new
-  end
-    
-  def after_register
-  end
-    
   def byebye
   end
 
-  def update_admin
-    @admin = Admin.where(vid_token: params[:v_id]).first
-      if @admin.update(admin_params)
-        if @admin.password.eql?(@admin.password_confirm)
-          # sign_in @admin
-          @admin.update(verification_code_confirm: true)
-          redirect_to edit_next_admin_path( @admin.vid_token)
-        else
-          flash[:notice] = "Password Doesnot match"
-          redirect_to edit_admin_path
-        end
-      else
-        flash[:alert]= @admin.errors.full_messages.to_sentence
-        redirect_to  edit_admin_path  
-      end
-  end
-  
-  def update_admin_details
-    @admin = Admin.where(vid_token: params[:v_id]).first
-
-      if @admin.update(admin_params)
-          sign_in @admin
-          redirect_to dash_admin_path
-      else
-        flash[:alert]= @admin.errors.full_messages.to_sentence
-        redirect_to  edit_admin_path  
-      end
-  end
-
-  def sign_up
-    @admin = Admin.where(vid_token: params[:v_id]).first
-  end
-  
-  def signup
-    @admin = Admin.where(vid_token: params[:v_id]).first
-  end
-  def price
-  end
-
-  def product
-  end
-  
   private 
     def check_sessions   
       return redirect_to dash_admin_path if current_admin.present?   
